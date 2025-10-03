ac3ab8076fc125efb510405d07b0a6982538681f
atmukesh01 <iammukesh1075@gmail.com>
2025-10-03 21:46:16 +0530

    Django

---
+++ manufacturing_dashboard_django/predictor/urls.py
@@ -0,0 +1,8 @@
+# File: predictor/urls.py
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.dashboard_view, name='dashboard'),
+]
--- manufacturing_dashboard_django/predictor/views.py
+++ manufacturing_dashboard_django/predictor/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+# File: predictor/views.py
 
-# Create your views here.
+from django.shortcuts import render
+from .ml_model import predict_quality # Import our prediction function
+
+def dashboard_view(request):
+    predicted_quality = None
+    
+    if request.method == 'POST':
+        # Get data from the form
+        temp = float(request.POST.get('temperature'))
+        pressure = float(request.POST.get('pressure'))
+        speed = float(request.POST.get('speed'))
+        
+        # Get prediction from our model
+        predicted_quality = predict_quality(temp, pressure, speed)
+    
+    # Prepare the context to pass to the template
+    context = {
+        'predicted_quality': predicted_quality
+    }
+    
+    return render(request, 'predictor/dashboard.html', context)
